6b96069aa34eb7f48b38a9451d9acece43b64471
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-08-28 08:47:58 +0000

    Validations to Event model attributes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,16 @@
 class Event < ApplicationRecord
   belongs_to :organizer, class_name: 'User'
-  has_many :attendances, foreign_key: :organized_event_id
-  has_many :event_attendees, through: :attendances, source: :attendee
+  has_many :attendances, foreign_key: :organized_event_id, dependent: :destroy
+  has_many :event_attendees, through: :attendances, source: :attendee, dependent: :destroy
+
+  scope :upcomming_events, -> { where('date >= ?', Time.zone.now)}
+  scope :previous_events, -> { where('date < ?', Time.zone.now)}
+  
+  # Ex:- scope :active, -> {where(:active => true)}
+
+  validates :title, presence: true, length: {maximum: 50, minimum: 5}
+  validates :location, presence: true
+  validates :date, presence: true
+  validates :details, presence: true, length: {maximum:250, minimum: 30}
+  
 end
