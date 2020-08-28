e01c3eb99d8c245f91112e70e1ce5d24d66f80b1
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-08-28 08:50:15 +0000

    Add helpr method to dynamic page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+    base_title = 'Ruby on Rails Active Record Associations Assignment - Microverse Curriculum'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
